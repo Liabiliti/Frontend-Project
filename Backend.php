6793ba2a5771a281b7e19784b6e528fdf4bf1408
Liabiliti <nicholas_b_01@hotmail.com>
2022-05-04 16:55:48 +1000

    Fixed broken links

--- Backend.php
+++ Backend.php
@@ -3,19 +3,49 @@
 <head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    body{
+      background:url('img1.jpg');
+    }
+    .container{
+      position: relative;
+      height: 90vh;
+      background-color: white;
+      top: 5vh;
+    }
+    .container h1{
+      text-align: center;
+      padding-top: 2.5vh;
+    }
+    .container .line{
+      height: 4px;
+      background-color: green;
+      width
+    }
+    .container form label{
+      left: 5vw;
+    }
+    .btn button{
+      background-color: green !important;
+    }
+    .container .TableContainer{
+      height: 50vh;
+      /*background-color: green;*/
+      border-radius: 5px;
+      border: 2px green solid;
+
+    }
+
+  </style>
 </head>
 <body>
   <div class="container">
-  <h1>PHP + MySql CRUD Demo</h1>
-  <p>Create, read, update, and delete records below</p>
-
-  <table class="table">
-    <tbody>
-      <?php include 'read.php'; ?>
-    </tbody>
-  </table>
-
-
+  <h1>Allergies</h1>
+  <div class="line"></div>
   <form class="form-inline m-2" action="create.php" method="POST">
     <label for="AllergyType">Allergy Type: </label>
       <input type="text" class = "form-control m-2" id="AllergyType" name="AllergyType" placeholder="Allergy Type">
@@ -27,7 +57,13 @@
       <input type="text" class = "form-control m-2" id="AllergyDescription" name="AllergyDescription" placeholder="Allergy Description">
       <button type="submit" class="btn btn-primary">Add</button>
   </form>
-
+  <div class="TableContainer">
+    <table class="table">
+      <tbody>
+        <?php include 'read.php'; ?>
+      </tbody>
+    </table>
+  </div>
   </div>
 </body>
 </html>
